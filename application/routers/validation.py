fff7e25296dba009b178fa3a4086a8e36b1547ad
George Goodall <ggoodall@scottlogic.com>
2023-05-10 15:55:54 +0100

    add basic report page

--- application/routers/validation.py
+++ application/routers/validation.py
@@ -2,6 +2,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, Request, File, UploadFile
 from application.core.utils import makeRequest
 from fastapi.responses import RedirectResponse
+import httpx
 
 templates = Jinja2Templates("application/templates")
 router = APIRouter()
@@ -20,9 +21,17 @@ async def upload(request: Request):
 async def uploadFile(file: UploadFile = File(...)):
     return RedirectResponse('/validation/report?filename='+file.filename, status_code=303)
 
+    # async with httpx.AsyncClient() as client:
+    #     response = await client.post('URL')
+        
+    # if(response.code == 200):
+    #     return RedirectResponse('/validation/report?filename='+file.filename, status_code=303)
+    # else:
+    #     return response.message
+
 @router.get('/report')
 async def report(request: Request):
-    template = 'validation/upload.html'
+    template = 'validation/report.html'
     context = {
         'request': request, 
     }
