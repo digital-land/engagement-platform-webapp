a5de94c62ee7edff0dc3d747de5c71756170ba68
George Goodall <ggoodall@scottlogic.com>
2023-05-10 15:54:25 +0100

    mount static files

--- application/app.py
+++ application/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from time import time
 import asyncio
 import json
@@ -8,6 +9,13 @@ from application.routers import (
 )
 
 app = FastAPI()
+
+# add static files
+app.mount(
+    "/static",
+    StaticFiles(directory="application/static"),
+    name="static",
+)
 
 cmsUrl = "http://localhost:8000/api/v2/pages/4/?format=json"
 conservationAreaUrl = "https://www.planning.data.gov.uk/dataset/conservation-area.json"
