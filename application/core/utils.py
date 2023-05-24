1bbaf6815630ac5f8180df4cc803daf4d59e936d
George Goodall <ggoodall@scottlogic.com>
2023-05-24 12:23:31 +0100

    linting changes

--- application/core/utils.py
+++ application/core/utils.py
@@ -1,28 +1,30 @@
 import json
 import httpx
 
-pagesUrl = 'http://localhost:8000/api/v2/pages/?format=json'
+pagesUrl = "http://localhost:8000/api/v2/pages/?format=json"
+
 
 async def makeRequest(url):
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         return response.text
-    
-    
+
+
 cmsUrl = "http://localhost:8000/api/v2/pages/{0}/?format=json"
+
 
 async def getPageContent(pageId):
     url = cmsUrl.format(pageId)
     response = await makeRequest(url)
     return json.loads(response)
-    
+
 
 async def getPageApiFromTitle(title):
     async with httpx.AsyncClient() as client:
         pagesResponse = await client.get(pagesUrl)
         pages = pagesResponse.json()
-        for page in pages['items']:
-            if(page['title'] == title):
-                pageId = page['id']
+        for page in pages["items"]:
+            if page["title"] == title:
+                pageId = page["id"]
                 break
     return await getPageContent(pageId)
