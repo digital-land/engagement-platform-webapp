25e1ed7a704b8dda1700d898e876f9c607022f55
George Goodall <ggoodall@scottlogic.com>
2023-05-30 14:57:26 +0100

    applied eslinting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-  },
-  plugins: [
-    'html',
-  ],
-};
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'airbnb-base',
+	overrides: [
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'no-restricted-syntax': 'off',
+		semi: ['error', 'never'],
+		'no-tabs': 'off',
+		indent: ['error', 'tab'],
+	},
+	plugins: [
+		'html',
+	],
+}
--- application/static/javascripts/map.js
+++ application/static/javascripts/map.js
@@ -1,111 +1,133 @@
-const entities = [];
+/* global L */
+
+const entities = []
 
 const calculateCentreAndBounds = (point1, point2) => {
-    let x = (parseInt(point1[0]) + parseInt(point2[0])) / 2
-    let y = (parseInt(point1[1]) + parseInt(point2[1])) / 2
+	const x = (parseInt(point1[0], 10) + parseInt(point2[0], 10)) / 2
+	const y = (parseInt(point1[1], 10) + parseInt(point2[1], 10)) / 2
 
-    let bounds = `[[${point1}], [${point2}]]`
+	const bounds = `[[${point1}], [${point2}]]`
 
-    return [`[${x},${y}]`, bounds]
+	return [`[${x},${y}]`, bounds]
 }
 
-let map;
-
-const initMap = () => {
-    map = L.map('map-1', {
-        attributionControl: false,
-        zoomControl: false,
-        dragging: true,
-        scrollWheelZoom: 'center'
-    })
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-
-    for(entity of entities){
-        var polygon = L.geoJSON(
-            {
-                "type": "Polygon",
-                "coordinates": JSON.parse(entity.geometry.area)
-            }).addTo(map);
-        entity.geometry.polygon = polygon
-    }
-
-    changeView(currentIndex)
-}
-
-let currentIndex = 0;
-
-const changeView = (index) => {
-    console.log(':: ' + index)
-    if(index >= entities.length){
-        index = 0
-    }
-    if(index < 0){
-        index = entities.length - 1
-    }
-    console.log('fitting bounds to ' + index)
-    map.fitBounds(JSON.parse(entities[index].geometry.bounds), {animate: true, duration: 0.5})
-    currentIndex = index;
-    resetAllHighlights()
-    highlightGeometry(index)
-    changeDetails(index)
-}
+let map
 
 const changeDetails = (index) => {
-    document.getElementById('entityName').innerHTML = `${entities[index].attributes['Reference']} - ${entities[index].attributes['Name']}`
-    attributes = '';
-    for(const[key,value] of Object.entries(entities[index].attributes)){
-        attributes += `<div class='attribute ${key.replace(' ', '_')}'>${key}: ${value}</div>`
-    }
+	document.getElementById('entityName').innerHTML = `${entities[index].attributes.Reference} - ${entities[index].attributes.Name}`
+	let attributes = ''
+	Object.entries(entities[index].attributes).forEach(([key, value]) => {
+		attributes += `<div class='attribute ${key.replace(' ', '_')}'>${key}: ${value}</div>`
+	})
 
-    document.getElementById('entityAttributes').innerHTML = attributes;
+	document.getElementById('entityAttributes').innerHTML = attributes
 
-    errors = '';
-    for(const[key,value] of Object.entries(entities[index].errors)){
-        errors += `<div>${key}: ${JSON.stringify(value)}</div>`
-    }
+	let errors = ''
+	Object.entries(entities[index].errors).forEach(([key, value]) => {
+		errors += `<div class='errors ${key.replace(' ', '_')}'>${key}: ${value}</div>`
+	})
 
-    document.getElementById('entityErrors').innerHTML = errors;
+	document.getElementById('entityErrors').innerHTML = errors
 }
 
-baseStyle = {
-    weight: 5,
-    color: '#8286ed',
-    pane: 'mapPane'
+const baseStyle = {
+	weight: 5,
+	color: '#8286ed',
+	pane: 'mapPane',
 }
 
 const resetAllHighlights = () => {
-    entities.forEach((entity) => {
-        entity.geometry.polygon.setStyle(baseStyle)
-    })
+	entities.forEach((entity) => {
+		entity.geometry.polygon.setStyle(baseStyle)
+	})
 }
 
 const highlightGeometry = (index) => {
-    map.removeLayer(entities[index].geometry.polygon)
+	map.removeLayer(entities[index].geometry.polygon)
 
-    console.log('adding')
+	console.log('adding')
 
-    var polygon = L.geoJSON(
-        {
-            "type": "Polygon",
-            "coordinates": JSON.parse(entities[index].geometry.area)
-        }).setStyle({
-            ...baseStyle,
-            color: '#FF0000',
-            pane: 'overlayPane'
-        }).addTo(map);
+	const polygon = L.geoJSON(
+		{
+			type: 'Polygon',
+			coordinates: JSON.parse(entities[index].geometry.area),
+		},
+	).setStyle({
+		...baseStyle,
+		color: '#FF0000',
+		pane: 'overlayPane',
+	}).addTo(map)
 
-    entities[index].geometry.polygon = polygon
+	entities[index].geometry.polygon = polygon
 }
 
-const geometry = (area, point, bounds, outsideUK) => {
-    return {
-        area,
-        point,
-        bounds,
-        outsideUK,
-    }
+/* eslint-disable no-unused-vars */
+const geometry = (area, point, bounds, outsideUK) => ({
+	area,
+	point,
+	bounds,
+	outsideUK,
+})
+
+let currentIndex = 0
+
+const changeView = (deltaIndex) => {
+	let newIndex = currentIndex + deltaIndex
+	if (newIndex >= entities.length) {
+		newIndex = 0
+	}
+	if (newIndex < 0) {
+		newIndex = entities.length - 1
+	}
+	if (entities[newIndex].geometry.outsideUK) {
+		const point = JSON.parse(entities[newIndex].geometry.point)
+		const [, bounds] = calculateCentreAndBounds(
+			[
+				point[1],
+				point[0],
+			],
+			[
+				52,
+				-2,
+			],
+		)
+		console.log(`fitting bounds to mid point of line ${newIndex}`)
+		map.fitBounds(JSON.parse(bounds), { animate: true, duration: 2 })
+	} else {
+		console.log(`fitting bounds to ${newIndex}`)
+		map.fitBounds(JSON.parse(entities[newIndex].geometry.bounds), { animate: true, duration: 2 })
+	}
+	currentIndex = newIndex
+	resetAllHighlights()
+	highlightGeometry(newIndex)
+	changeDetails(newIndex)
+}
+
+/* eslint-disable no-unused-vars */
+const initMap = () => {
+	map = L.map('map-1', {
+		attributionControl: false,
+		zoomControl: false,
+		dragging: true,
+		scrollWheelZoom: 'center',
+	})
+
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	}).addTo(map)
+
+	entities.map((entity) => {
+		const newEntity = { ...entity }
+		const polygon = L.geoJSON(
+			{
+				type: 'Polygon',
+				coordinates: JSON.parse(newEntity.geometry.area),
+			},
+		).addTo(map)
+		newEntity.geometry.polygon = polygon
+		return newEntity
+	})
+
+	changeView(0)
 }
